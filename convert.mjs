77000f909eb6cc87ce2aac262035ee9938b7ea11
Nicolás Deyros <ndeyros@gmail.com>
2026-02-02 18:04:46 -0300

    chore: remove leftover conversion script

--- convert.mjs
+++
@@ -1,9 +0,0 @@
-import pngToIco from 'png-to-ico';
-import fs from 'fs';
-
-pngToIco('public/favicon.png')
-  .then(buf => {
-    fs.writeFileSync('public/favicon.ico', buf);
-    console.log('Created public/favicon.ico');
-  })
-  .catch(console.error);
